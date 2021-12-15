0b49b648a2015dcb0dda56236e22cbb600ee51a8
Ông Triệu Hậu <58720300+o-ngtrieuhau861gmailcom@users.noreply.github.com>
2021-12-15 16:07:50 +0700

    Nén file rar, có tên là dạng version chính của module printer

--- Deploy_Tools/compressRar.ps1
+++ Deploy_Tools/compressRar.ps1
@@ -1,14 +1,19 @@
 param (
-    $SourcePathFile
+    $SourcePathFile,
+    $ExeName
 )
+Write-Output $SourcePathFile, $ExeName
 $PathSource = $SourcePathFile
 $DirectorySource = [System.IO.Path]::GetDirectoryName($SourcePathFile)
 $DirectorySource_0 = [System.IO.Path]::GetDirectoryName($DirectorySource)
+$PathExe = [System.IO.Path]::GetDirectoryName($DirectorySource_0) + "\Deploy_Libraries\" + $ExeName
+Write-Output $PathExe 
+$VersionExe = [System.Diagnostics.FileVersionInfo]::GetVersionInfo($PathExe).FileVersion
 
 $DestNameFile = [System.IO.Path]::GetFileNameWithoutExtension($SourcePathFile)
-$DestPathFile = $DirectorySource + "\" + $DestNameFile
+$DestPathFile = $DirectorySource + "\" + $DestNameFile + ".v" + $VersionExe + ".rar"
 
 $rarExe = $DirectorySource_0 + "\Rar.exe"
 
-$argList = @("a -agYYYYMMDD -r -m5 -rr8 -ep", ('"' + $DestPathFile + '"'), ('"' + $PathSource + '"'))
+$argList = @("a -r -m5 -rr8 -ep", ('"' + $DestPathFile + '"'), ('"' + $PathSource + '"'))
 Start-Process -FilePath $rarExe -ArgumentList $argList -NoNewWindow -Wait
